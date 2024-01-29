a01f5344424512c6fc29628d20071a3539f70c26
Amudha Balamurugan <100310286+DevABM@users.noreply.github.com>
2024-01-29 05:51:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,64 @@
-// create a web server
+// create web server
 
-// 1. require the express module
-const express = require('express');
-const router = express.Router();
-// 2. create an instance of an express server
-const server = express();
-// 3. require the json data
-const comments = require('../data/comments');
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const { comments } = require("./data");
 
-// 4. create a route
-router.get('/comments', (req, res) => {
-    res.json(comments);
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// GET request
+app.get("/api/comments", (req, res) => {
+  res.status(200).json(comments);
 });
 
-// 5. export the router
-module.exports = router;
+// POST request
+app.post("/api/comments", (req, res) => {
+  const { body } = req;
+  if (body.name && body.comment) {
+    const newComment = {
+      id: comments.length + 1,
+      name: body.name,
+      comment: body.comment,
+    };
+    comments.push(newComment);
+    res.status(201).json(newComment);
+  } else {
+    res.status(400).json({ msg: "Please enter name and comment" });
+  }
+});
+
+// PUT request
+app.put("/api/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  const comment = comments.find((comment) => comment.id === Number(id));
+  if (comment) {
+    const index = comments.indexOf(comment);
+    comments[index] = { ...comment, ...body };
+    res.status(200).json(comments[index]);
+  } else {
+    res.status(404).json({ msg: `Comment not found with id of ${id}` });
+  }
+});
+
+// DELETE request
+app.delete("/api/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((comment) => comment.id === Number(id));
+  if (comment) {
+    comments = comments.filter((comment) => comment.id !== Number(id));
+    res.status(200).json({ msg: `Comment deleted with id of ${id}` });
+  } else {
+    res.status(404).json({ msg: `Comment not found with id of ${id}` });
+  }
+});
+
+// listen on port 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+
